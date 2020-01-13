af7b6e7453185ace497146e507a6af31ab750932
joffitzer <joffitzer@gmail.com>
2020-01-13 17:15:21 -0500

    Done.

--- src/components/PokemonIndex.js
+++ src/components/PokemonIndex.js
@@ -5,16 +5,152 @@ import Search from './Search'
 import { Container } from 'semantic-ui-react'
 
 class PokemonPage extends React.Component {
+
+  state = {
+    allPokemon: [],
+    searchTerm: "",
+    name: '',
+    hp: '',
+    frontUrl: '',
+    backUrl: '',
+    sorted: false
+  }
+
+  componentDidMount() {
+    fetch ('http://localhost:3000/pokemon')
+      .then(res => res.json())
+      .then(pokemon => this.setState({
+        allPokemon: pokemon
+      }))
+  }
+
+  handleSearch = (e) => {
+    this.setState({
+      searchTerm: e.target.value
+    })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('http://localhost:3000/pokemon', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json', 
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify({
+        name: this.state.name, 
+        hp: this.state.hp,
+        frontUrl: this.state.frontUrl,
+        backUrl: this.state.backUrl
+      })
+    })
+    .then( () => {
+      fetch ('http://localhost:3000/pokemon')
+      .then(res => res.json())
+      .then(pokemon => this.setState({
+        allPokemon: pokemon
+      }))
+    }
+    )
+  }
+
+handleNameChange = (e) => {
+  this.setState({
+    name: e.target.value
+  })
+}
+
+handleHpChange = (e) => {
+  this.setState({
+    hp: e.target.value
+  })
+}
+
+handleFrontChange = (e) => {
+  this.setState({
+    frontUrl: e.target.value
+  })
+}
+
+handleBackChange = (e) => {
+  this.setState({
+    backUrl: e.target.value
+  })
+}
+
+handleSubmit = () => {
+  fetch('http://localhost:3000/pokemon', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json', 
+      'Accept': 'application/json'
+    },
+    body: JSON.stringify({
+      name: this.state.name, 
+      hp: this.state.hp,
+      frontUrl: this.state.frontUrl,
+      backUrl: this.state.backUrl
+    })
+  })
+  .then( () => {
+    fetch ('http://localhost:3000/pokemon')
+    .then(res => res.json())
+    .then(pokemon => this.setState({
+      allPokemon: pokemon
+    }))
+  }
+  ).then(
+    this.setState({
+      name: '',
+      hp: '',
+      frontUrl: '',
+      backUrl: ''
+    })
+  )
+}
+
+handleAlphaSort = () => {
+  this.setState(prevState => ({
+    sorted: !prevState.sorted
+  }));
+}
+
   render() {
+
+    let lowerCasedSearch = this.state.searchTerm.toLowerCase();
+
+    let filteredPokemon = this.state.allPokemon.filter(pokemon => {
+      return lowerCasedSearch.includes(pokemon.name)
+    }
+    )
+
+    let alphaPokemon = [...this.state.allPokemon]
+    alphaPokemon.sort(function (a, b) {
+      if (a.name < b.name) return -1;
+      else if (a.name > b.name) return 1;
+      return 0;
+    });
+
     return (
       <Container>
         <h1>Pokemon Searcher</h1>
         <br />
-        <PokemonForm />
+        <PokemonForm 
+        name={this.state.name}
+        hp={this.state.hp}
+        frontUrl={this.state.frontUrl}
+        backUrl={this.state.backUrl}
+        handleNameChange={this.handleNameChange} 
+        handleHpChange={this.handleHpChange}
+        handleFrontChange={this.handleFrontChange}
+        handleBackChange={this.handleBackChange}
+        handleSubmit={this.handleSubmit}/>
         <br />
-        <Search onChange={() => console.log('🤔')} />
+        <Search searchTerm={this.state.searchTerm} handleSearch={this.handleSearch} />
         <br />
-        <PokemonCollection />
+        <button onClick={this.handleAlphaSort}>Sort Alphabetically</button><br></br>
+        <PokemonCollection sorted={this.state.sorted} alphaPokemon={alphaPokemon} allPokemon={this.state.searchTerm === "" ? this.state.allPokemon : filteredPokemon} sortedPokemon={alphaPokemon}/>
       </Container>
     )
   }
