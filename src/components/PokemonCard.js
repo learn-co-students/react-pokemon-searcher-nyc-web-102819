6ca62b29047a05589c4b5fa67d57cec69ade004d
hdp2112 <hdp2112@columbia.edu>
2020-01-14 09:30:31 -0500

    Done.

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -2,22 +2,60 @@ import React from 'react'
 import { Card } from 'semantic-ui-react'
 
 class PokemonCard extends React.Component {
+
+  state = {
+    cardClicked: false
+  }
+
+  handleClick = () => {
+    this.setState({
+      cardClicked: !this.state.cardClicked
+    })
+  }
+
+  cards = () => {
+    if (this.state.cardClicked === true) {
+    return <Card>
+        <div onClick={this.handleClick}>
+      <div className="image">
+        <img src={this.props.back} alt="oh no!" />
+      </div>
+      <div className="content">
+        <div className="header">{this.props.name}</div>
+      </div>
+      <div className="extra content">
+        <span>
+          <i className="icon heartbeat red" />
+          {this.props.hp} hp
+        </span>
+      </div>
+    </div>
+    </Card>
+    } else if (this.state.cardClicked === false) {
+        return <Card>
+          <div onClick={this.handleClick}>
+      <div className="image">
+        <img src={this.props.front} alt="oh no!" />
+      </div>
+      <div className="content">
+        <div className="header">{this.props.name}</div>
+      </div>
+      <div className="extra content">
+        <span>
+          <i className="icon heartbeat red" />
+          {this.props.hp} hp
+        </span>
+      </div>
+    </div>
+  </Card>
+    }
+  }
+
   render() {
     return (
       <Card>
-        <div>
-          <div className="image">
-            <img alt="oh no!" />
-          </div>
-          <div className="content">
-            <div className="header">POKEMON NAME HERE</div>
-          </div>
-          <div className="extra content">
-            <span>
-              <i className="icon heartbeat red" />
-              POKEMON HP HERE hp
-            </span>
-          </div>
+        <div onClick={this.handleClick}>
+          {this.cards()}
         </div>
       </Card>
     )
